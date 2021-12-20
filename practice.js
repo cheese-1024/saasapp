d1b3f9265f698e91ab0ae4605df9e31547c19956
Tommy Coleman <95910473+gjasklgjasdklg@users.noreply.github.com>
2021-12-20 14:21:08 +0000

    Revert "wtf"
    This reverts commit 64edbbbc0a0d85a8acac7bdd767c812e266863d0.

--- practice.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("sideNavbar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-    document.getElementById("sideNavbar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
